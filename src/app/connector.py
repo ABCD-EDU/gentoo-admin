2bdabec055ca9d6b8afc4b86d649f74b34ad3773
Christian Bayquen <90618180+chrisgabs@users.noreply.github.com>
2022-05-02 03:05:57 +0800

    add: initial ini file

--- src/app/connector.py
+++ src/app/connector.py
@@ -1,1 +1,9 @@
 # File used to connect to database
+
+import configparser
+Config = configparser.ConfigParser()
+
+Config.read("../../config.ini")
+DB_USR = Config.get('DB', 'USR')
+DB_PW = Config.get('DB', 'PW')
+print(DB_USR, DB_PW)

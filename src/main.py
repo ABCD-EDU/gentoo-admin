450db4fcae73f1761c7748b3899069298f4b23d8
Christian Bayquen <90618180+chrisgabs@users.noreply.github.com>
2022-05-02 11:52:19 +0800

    update, fix: deleted test.py, exception handling for connector.py, fixed indent spacing in main.py

--- src/main.py
+++ src/main.py
@@ -7,29 +7,29 @@ conn = connect()
 
 @app.get("/")
 async def root():
-        return {"message": "Hello World"}
+	return {"message": "Hello World"}
 
 @app.get("/posts/{text}")
 async def return_text(text: str) -> str:
-        return {"text": text}
+    return {"text": text}
 
 @app.get("/get-data")
 def get_datadb():
-        cur = conn.cursor()
-        data = get_data(cur)
-        cur.close()
-        return data
+	cur = conn.cursor()
+	data = get_data(cur)
+	cur.close()
+	return data
 
 @app.get("/fun1")
 def get_datadb():
-        cur = conn.cursor()
-        data = fun1(cur)
-        cur.close()
-        return data
+	cur = conn.cursor()
+	data = fun1(cur)
+	cur.close()
+	return data
 
 @app.get("/fun2")
 def get_datadb():
-        cur = conn.cursor()
-        data = fun2(cur)
-        cur.close()
-        return data
+	cur = conn.cursor()
+	data = fun2(cur)
+	cur.close()
+	return data
--- src/test.py
+++
@@ -1,9 +0,0 @@
-from app.processing import *
-from app.connector import connect
-
-conn = connect()
-cur = conn.cursor()
-data = get_data(cur)
-
-print(fun1(cur))
-print(fun2(cur))

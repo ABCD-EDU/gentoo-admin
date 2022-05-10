ee64ba7839340efc168acaf28919a235c6934cb6
Christian Bayquen <90618180+chrisgabs@users.noreply.github.com>
2022-05-10 10:45:44 +0800

    fix: CORS requests now granted for specified origins

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
 from app.processing import *
 from app.connector import connect
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 conn = connect()
+
+# ADD ADDRESS OF FRONTEND
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
